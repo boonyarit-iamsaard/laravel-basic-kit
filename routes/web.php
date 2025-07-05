848d723e89d1a482b50b8bb5cd3ad78391b974da
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2025-07-05 19:57:03 +0700

    refactor: rename welcome view to home

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,11 @@ declare(strict_types=1);
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', fn () => view('welcome'));
+Route::get('/', fn () => view('home'))->name('home');
 
-Route::get('/dashboard', fn () => view('dashboard'))->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function (): void {
+    Route::get('/dashboard', fn () => view('dashboard'))->name('dashboard');
 
-Route::middleware('auth')->group(function (): void {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
