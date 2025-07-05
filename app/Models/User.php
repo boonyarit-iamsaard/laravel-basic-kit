c071237701b4687f195b8ab68b507045f1856cf6
Boonyarit Iamsa-ard <boonyarit.iamsaard@gmail.com>
2025-07-05 15:42:45 +0700

    chore: configure phpstan

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,14 +4,20 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-final class User extends Authenticatable
+/**
+ * @use HasFactory<UserFactory>
+ */
+final class User extends Authenticatable implements MustVerifyEmail
 {
+    /** @use HasFactory<UserFactory> */
     use HasFactory;
+
     use Notifiable;
 
     /**
